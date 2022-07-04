e7d65d2480d40c2a13dc6bfdbd7674a5160cc440
mincheolkk <kmc04080408@gmail.com>
2022-07-04 21:53:36 +0900

    차트 띄우기 성공, 하지만 리스트로 여러개 띄우는건 실패

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,104 +1,128 @@
 <template>
-    <canvas id="myChart" width="400" height="400"></canvas>
     <h1> 개발자의 서재</h1>
-    <div v-for="(result) in this.min" v-bind:key="result">
+    <div v-for="(result) in this.min" v-bind:key="result" class="shadow">
         <img :src="result.detailData.thumbnail" alt="image"/>
         <p>{{result.detailData.authors}}</p>
+        <p>{{result}}</p>
         <div>{{result.readTimeMap}}</div>
-  </div>
+        <div>
+            <canvas ref="barChart"/>
+        </div>
+
+    </div>
+    
+    
   
 </template>
 
-<script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.1/dist/chart.min.js">
+<script>
 import axios from "axios";
+import { Chart, registerables } from 'chart.js'
+Chart.register(...registerables)
 
-const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+let caseChart;
 
 export default {   
-    mounted() {
-        this.ttData();
+ 
+  async mounted(){
+    this.$nextTick(function () {
+      this.ttData();
+      this.createChart()
+    })
   },
+
 
   data() {
     return {
       min:'',
-      categories: ["June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"],
-  series: [
-    {
-      name: "Budget",
-      data: [5000, 3000, 5000, 7000, 6000, 4000, 1000]
-    },
-    {
-      name: "Income",
-      data: [8000, 4000, 7000, 2000, 6000, 3000, 5000]
-    },
-    {
-      name: "Expenses",
-      data: [4000, 4000, 6000, 3000, 4000, 5000, 7000]
-    },
-    {
-      name: "Debt",
-      data: [3000, 4000, 3000, 1000, 2000, 4000, 3000]
+
+      data:  {
+                labels:['a'],
+                datasets:[
+                    {
+                        label:"ks",
+                        data:[42],
+                        backgroundColor:"#4169e1"
+                    },
+                    {
+                        label:"m",
+                        data:[48],
+                        backgroundColor:"#87ceeb"
+                    },
+                    {
+                        label:'c',
+                        data:[39],
+                        backgroundColor:"#b0e0e6"
+                    },
+                    {
+                        label:'d',
+                        data:[1],
+                        backgroundColor:"#b33e22"
+                    }
+                ]
+            
+            },
+    options: {
+        scales: {
+            x:{
+                stacked:true,
+                display:false
+            },
+            y: {
+                stacked:true,
+                display:false
+            }
+        },
+        indexAxis:'y',
+        plugins:{
+            legend:{
+                display:true
+            }
+        }
     }
-  ]
+    
     }
   },
+  
 
-  methods: {
-    ttData() {  
-             axios
+    methods: {
+   async ttData() {  
+             await axios
                 .get('https://438d14e0-58fe-4eb2-8704-c2b65596f942.mock.pstmn.io/test')
                 .then(res => {
                         console.log(res.data.resultList);
                         this.min = res.data.resultList;
-                        for (var i=0; i<this.min.length; i++) {
-                            console.log(this.min[0].readTimeMap)
-                        }
-
                     })
                 .catch(error => {
                         console.log(error)
                 })
-         }
-  },
+         },
+
+    createChart() {
+        console.log(caseChart);
+        if (caseChart !== undefined){
+            caseChart.destroy();
+        }
+
+      caseChart = new Chart(this.$refs.barChart, {
+        type: 'bar',
+        data: this.data,
+        options: this.options
+      })
+      console.log(caseChart);
+    },
+
+    }
+  
+    
+
 
 }
 </script>
 
 <style>
-
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
 </style>
+
